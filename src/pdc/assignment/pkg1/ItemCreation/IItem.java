2fd809d6f56e1b116d4696c526c2e9636fac33b5
Weaselcore <weaselcorp@hotmail.com>
2021-04-09 12:22:49 +1200

    Created item factory

---
+++ src/pdc/assignment/pkg1/ItemCreation/IItem.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdc.assignment.pkg1.ItemCreation;
+
+/**
+ *
+ * @author Weaselcore
+ */
+public interface IItem {
+    
+    public void Create();
+    
+}
