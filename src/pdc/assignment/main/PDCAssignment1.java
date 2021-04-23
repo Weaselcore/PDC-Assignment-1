251c82f86f29232881f2316d34658899ae68ef52
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2021-04-23 18:28:00 +1200

    Fixed file selection bug.

--- src/pdc/assignment/main/PDCAssignment1.java
+++ src/pdc/assignment/main/PDCAssignment1.java
@@ -17,57 +17,65 @@ public class PDCAssignment1 {
 
     /**
      * Entry point to start a game.
+     *
      * @param args
      * @throws java.lang.Exception
      */
     public static void main(String[] args) throws Exception {
-        
+
         Boolean isRunning = true;
         Scanner input = new Scanner(System.in);
-        File saveFolder = new File("src/pdc/assignment/saves/");
-        
+        String saveFolderString = "src/pdc/assignment/saves/";
+        File saveFolder = new File(saveFolderString);
+
         while (isRunning) {
-            
+
             System.out.println("KINGDOM FIGHTERS\n");
             System.out.println("[#1: New Game]");
             System.out.println("[#2: Load Game]");
-            System.out.println("[#3: Delete Save File]");           
+            System.out.println("[#3: Delete Save File]");
             System.out.println("[#4: Exit]");
             System.out.print("Input: ");
-            
+
             String result = input.nextLine();
-            
-            if ("1".equals(result)) {             
+
+            if ("1".equals(result)) {
                 GameSession gameSession = new GameSession();
                 break;
             } else if ("2".equals(result)) {
-                
-                File fileResult = getSaveFile(saveFolder);
-                GameSession gameSession = new GameSession(fileResult);
 
-            } else if ("3".equals(result)){
-                
+                File fileResult = getSaveFile(saveFolder);
+                if (null != fileResult) {
+                    GameSession gameSession = new GameSession(fileResult);
+                } else {
+                    System.out.println("Please input a valid file number.");
+                }
+
+            } else if ("3".equals(result)) {
+
                 try {
                     File fileResult = getSaveFile(saveFolder);
-                    if (!(fileResult == null)) {
+                    if (null != fileResult) {
                         if (fileResult.delete()) {
                             System.out.println("File has been deleted.");
                         } else {
                             System.out.println("File failed to be deleted.");
                         }
+                    } else {
+                        System.out.println("Please use a valid input.");
                     }
-                }   catch(Exception e) {
-                        System.out.println(e);
+                } catch (Exception e) {
+                    System.out.println(e);
                 }
-            } else if ("4".equals(result)){
+            } else if ("4".equals(result)) {
                 System.out.println("Thanks for playing!\n");
-                System.exit(0);    
+                System.exit(0);
             } else {
                 System.out.println("Please input a proper option!");
             }
-        } 
-    } 
-    
+        }
+    }
+
     // Used to retrieve list of saves for loading and deletion of files.
     public static File getSaveFile(File saveFolder) throws Exception {
 
@@ -90,23 +98,21 @@ public class PDCAssignment1 {
             int count = 0;
 
             for (File file : listOfSaves) {
-                if (file.isFile()){
-                    System.out.println("[#" + (count + 1) +": " + file.getName() + "]");
+                if (file.isFile()) {
+                    System.out.println("[#" + (count + 1) + ": " + file.getName() + "]");
                     saveFileList.add(file);
                     count += 1;
                 }
             }
 
             int saveChoice = scanner.nextInt();
-            
+
             if (saveChoice <= count) {
                 return (File) saveFileList.get(saveChoice - 1);
             } else {
-                throw new Exception("Please input a valid number.");
+                return null;
             }
-
         }
         return null;
     }
-   
 }
